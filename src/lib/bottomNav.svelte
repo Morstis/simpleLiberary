643f7b9e838baecca7d872771e74594446b959a5
Lucas Wiese <lucas.wiese@gmx.de>
2023-04-12 16:52:32 +0200

    bottom nav + deps

---
+++ src/lib/bottomNav.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	import Icon from 'svelte-icons-pack/Icon.svelte';
+	import IoSearchSharp from 'svelte-icons-pack/io/IoSearchSharp';
+	import IoLibrary from 'svelte-icons-pack/io/IoLibrary';
+</script>
+
+<div id="nav">
+	<div>
+		<Icon src={IoSearchSharp} size="32" className="menuIcon" color="var(--secondaryText)" />
+		<span>Suchen</span>
+	</div>
+	<div>
+		<Icon src={IoLibrary} size="32" className="menuIcon" color="var(--secondaryText)" />
+		<span>Bibliothek</span>
+	</div>
+</div>
+
+<style lang="scss">
+	#nav {
+		display: flex;
+		background-color: var(--secondary);
+		padding: 1em;
+		border-radius: 10px 10px 0 0;
+		justify-content: space-around;
+		bottom: 0;
+		position: absolute;
+		width: 100%;
+		div {
+			display: flex;
+			flex-direction: column;
+			span {
+				color: var(--secondaryText);
+			}
+		}
+	}
+	:global(.menuIcon) {
+		margin: auto;
+	}
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import '../app.scss';
+	import BottomNav from '$lib/bottomNav.svelte';
+</script>
+
+<main>
+	<slot />
+</main>
+<BottomNav />
+
+<style lang="scss">
+	main {
+		height: 100%;
+		width: 100%;
+	}
+</style>
