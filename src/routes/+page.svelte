0de9901f6e346f823aa5fb9152f4320b851be452
Lucas Wiese <lucas.wiese@gmx.de>
2023-04-13 00:37:12 +0200

    added capacitor

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,26 @@
-home
+<h1 class="headline">Eine einfache Bibliothek</h1>
+
+<section>
+	<a href="/search"><span>Suchen</span></a>
+	<a href="/library"><span>Bibliothek</span></a>
+</section>
+
+<style lang="scss">
+	section {
+		display: flex;
+		margin-top: 1em;
+		height: 100%;
+		flex-direction: column;
+		gap: 1em;
+		a {
+			text-decoration: none;
+			font-size: 1.4em;
+			background-color: var(--accent);
+			border-radius: 10px;
+			height: 100%;
+			display: flex;
+			align-items: center;
+			justify-content: center;
+		}
+	}
+</style>
